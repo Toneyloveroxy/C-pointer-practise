ba1a63aba42817abe4956026a05f49f4643301da
Toneyloveroxy <yikchilong2007@gmail.com>
2026-05-27 04:16:11 +0800

    Add pointer practice files and .gitignore

---
+++ Empty_pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Empty pointer
+    // 1.Use to initializate the pointer varible
+    //int * p = NULL;
+
+    // 2.Empty pointer are not allowed to visit
+    // The memory number between 0 and 225 are used by system so it is not allowed to visit
+    //* p = 100;
+
+    return 0;
+}
--- Memory_space.cpp
+++ Memory_space.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main() {
-    
+    int a = 10;
+    int * p = &a;
 
+    //Under 32 bit, pointer have 4 unit space size no matter the data
+    //Under 64 bit, pointer have 8 unit space size type
+    cout << "Size of int * = " << sizeof(int *) << endl;
+    cout << "Size of int * = " << sizeof(float *) << endl;
+    cout << "Size of int * = " << sizeof(double *) << endl;
+    cout << "Size of int * = " << sizeof(char *) << endl;
+    
     return 0;
 }
